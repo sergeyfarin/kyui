2a27823ed135058a7fc39c91511f0cd7126c85d2
mnijph@gmail.com <mnijph@gmail.com@61556d0e-e001-f3ff-fadc-bb871643678f>
2010-12-09 16:56:14 +0000

    Made a few changes to ToolGroup, started work on a modified ToolButton. Cleaned up Style.pyw and added ToolButton painter methods. Added scribble.cpp for misc. C++ scratchpadding.

---
+++ TestItems/scribble.cpp
@@ -0,0 +1,16 @@
+enum {
+	TP_BUTTON = 1,
+	TP_DROPDOWNBUTTON = 2,
+	TP_SPLITBUTTON = 3,
+	TP_SPLITBUTTONDROPDOWN = 4,
+	TP_SEPARATOR = 5,
+	TP_SEPARATORVERT = 6
+};
+enum {
+	TS_NORMAL = 1,      //inactive
+	TS_HOT = 2,         //hover
+	TS_PRESSED = 3,     //down
+	TS_DISABLED = 4,    //
+	TS_CHECKED = 5,     //
+	TS_HOTCHECKED = 6   //checked and hovered
+};
