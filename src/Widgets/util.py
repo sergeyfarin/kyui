269cbf46638e73cc333a22977f3c17f80e8bf0dd
mnijph@gmail.com <mnijph@gmail.com@61556d0e-e001-f3ff-fadc-bb871643678f>
2011-10-13 08:40:51 +0000

    Cleaned up util.py, separating Q[x]-class-to-string methods and Q[x]-class-to-icon methods into different classes. Removed some unnecessary imports.

--- src/Widgets/util.py
+++ src/Widgets/util.py
@@ -1,9 +1,40 @@
-from PyQt4.QtCore import Qt, QLocale, QSize, qWarning
-from PyQt4.QtGui import QApplication, QKeySequence
+from PyQt4.QtCore import Qt, QLocale, QSize
+from PyQt4.QtGui import QApplication
 from PyQt4.QtGui import QBoxLayout, QSizePolicy
-from PyQt4.QtGui import QCursor, QFont
 from PyQt4.QtGui import QPixmap, QIcon
-from PyQt4.QtGui import QColor, QPalette, QPolygon
+from PyQt4.QtGui import QColor
+
+SizePolicies = {0 : 'Fixed', 
+                1 : 'Minimum', 
+                4 : 'Maximum', 
+                5 : 'Preferred', 
+                7 : 'Expanding', 
+                3 : 'MinimumExpanding', 
+                13 : 'Ignored'}
+
+Cursors = { Qt.ArrowCursor : 'ArrowCursor', 
+            Qt.UpArrowCursor : 'UpArrowCursor', 
+            Qt.CrossCursor : 'CrossCursor', 
+            Qt.WaitCursor : 'WaitCursor', 
+            Qt.IBeamCursor : 'IBeamCursor', 
+            Qt.SizeVerCursor : 'SizeVerCursor', 
+            Qt.SizeHorCursor : 'SizeHorCursor', 
+            Qt.SizeBDiagCursor : 'SizeBDiagCursor', 
+            Qt.SizeFDiagCursor : 'SizeFDiagCursor', 
+            Qt.SizeAllCursor : 'SizeAllCursor', 
+            Qt.BlankCursor : 'BlankCursor', 
+            Qt.SplitVCursor : 'SplitVCursor', 
+            Qt.SplitHCursor : 'SplitHCursor', 
+            Qt.PointingHandCursor : 'PointingHandCursor', 
+            Qt.ForbiddenCursor : 'ForbiddenCursor', 
+            Qt.OpenHandCursor : 'OpenHandCursor', 
+            Qt.ClosedHandCursor : 'ClosedHandCursor', 
+            Qt.WhatsThisCursor : 'WhatsThisCursor', 
+            Qt.BusyCursor : 'BusyCursor', 
+            Qt.DragMoveCursor : 'DragMoveCursor', 
+            Qt.DragCopyCursor : 'DragCopyCursor', 
+            Qt.DragLinkCursor : 'DragLinkCursor', 
+            Qt.BitmapCursor : 'BitmapCursor'}
 
 class Util():
     @staticmethod
@@ -14,7 +45,7 @@ class Util():
             (colorA.blue() * factor) / 100 + (colorB.blue() * (100 - factor)) / 100)
     
     @staticmethod
-    def directionFromOrientation(orientation : Qt.Orientation) -> QBoxLayout.Direction:
+    def directionFromOrientation(orientation : Qt.Orientation):
         if orientation == Qt.Vertical:
             return QBoxLayout.TopToBottom
         elif QApplication.isLeftToRight():
@@ -23,83 +54,134 @@ class Util():
             return QBoxLayout.RightToLeft
     
     @staticmethod
-    def orientationFromDirection(direction : QBoxLayout.Direction) -> Qt.Orientation:
+    def orientationFromDirection(direction : QBoxLayout.Direction):
         if (direction == QBoxLayout.TopToBottom 
             or direction == QBoxLayout.BottomToTop):
             return Qt.Vertical
         else:
             return Qt.Horizontal
-        
-class FormatQType():
+
+class QTypeToIcon():
     @staticmethod
-    def noneType(value) -> str:
+    def noneType(value):
         return ''
-    
-    @staticmethod
-    def cursor(value : QCursor) -> str:
-        return Cursors[int(value.shape())]
-    
-    @staticmethod
-    def sizepolicy(value : QSizePolicy) -> str:
-        horiz = QSizePolicy.Policy(value.horizontalPolicy())
-        vert = QSizePolicy.Policy(value.verticalPolicy())
-        return '{}, {}, Stretch: {} x {}'.format(SizePolicies[horiz], 
-                                                 SizePolicies[vert], 
-                                                 value.horizontalStretch(), 
-                                                 value.verticalStretch())
-    @staticmethod
-    def font(value : QFont) -> str:
-        return '{}, {}pt, Bold: {}, Italic: {}'.format(value.family(), 
-                                                       value.pointSize(), 
-                                                       value.bold(), 
-                                                       value.italic())
 
+    #QIcon
     @staticmethod
-    def keysequence(value: QKeySequence) -> str:
-        return value.toString()
-
-    @staticmethod
-    def locale(value : QLocale) -> str:
-        return '{}, {}'.format(QLocale.languageToString(value.language()), 
-                               QLocale.countryToString(value.country()))
-
-    @staticmethod
-    def size(value : QSize) -> str:
-        if not value.isValid():
-            return '0 x 0'
-        return '{} x {}'.format(value.width(), value.height())
-
-    @staticmethod
-    def point(value : QSize) -> str:
-        return '({}, {})'.format(value.x(), value.y())
-
-    @staticmethod
-    def line(value : QSize) -> str:
-        return '({}, {}), ({}, {})'.format(value.x1(), value.y1(), value.x2(), value.y2())
-
-    @staticmethod
-    def rect(value : QSize) -> str:
-        return '({}, {}), {} x {}'.format(value.x(), value.y(), value.width(), value.height())
-
-    @staticmethod
-    def icon(value : QIcon) -> QIcon:
+    def icon(value):
         return value
     
+    #QPixmap
     @staticmethod
-    def pixmap(value : QPixmap) -> QIcon:
+    def pixmap(value):
         return QIcon(value)
-
+    
+    #QColor
     @staticmethod
-    def color(value : QColor, size = QSize(16, 16)):
+    def color(value, size = QSize(16, 16)):
         pixmap = QPixmap(size)
         pixmap.fill(value)
         return QIcon(pixmap)
-
+    
+    #QCursor
     @staticmethod
-    def icon_qcursor(value : QCursor):
+    def cursor(value):
         pixmap = value.pixmap()
         if not pixmap.isNull():
             icon = QIcon(pixmap)
         else:
             icon = QIcon(value.bitmap())
         return icon
+
+class QTypeToString():
+    @staticmethod
+    def noneType(value):
+        return ''
+    
+    #QCursor
+    @staticmethod
+    def cursor(value):
+        return Cursors[value.shape()]
+    
+    #QSizePolicy
+    @staticmethod
+    def sizepolicy(value):
+        horiz = QSizePolicy.Policy(value.horizontalPolicy())
+        vert = QSizePolicy.Policy(value.verticalPolicy())
+        return '{}, {}, Stretch: {} x {}'.format(SizePolicies[horiz], 
+                                                 SizePolicies[vert], 
+                                                 value.horizontalStretch(), 
+                                                 value.verticalStretch())
+    #QFont
+    @staticmethod
+    def font(value):
+        return '{}, {}pt, Bold: {}, Italic: {}'.format(value.family(), 
+                                                       value.pointSize(), 
+                                                       value.bold(), 
+                                                       value.italic())
+    
+    #QKeySequence
+    @staticmethod
+    def keysequence(value):
+        return value.toString()
+    
+    #QLocale
+    @staticmethod
+    def locale(value):
+        return '{}, {}'.format(QLocale.languageToString(value.language()), 
+                               QLocale.countryToString(value.country()))
+    
+    #QMargins
+    @staticmethod
+    def margins(value):
+        if value.isNull():
+            return '(Null)'
+        return '({}, {}, {}, {})'.format(value.left(), value.top(), 
+                                         value.right(), value.bottom())
+    #QSize
+    @staticmethod
+    def size(value):
+        if not value.isValid():
+            return '0 x 0'
+        return '{} x {}'.format(value.width(), value.height())
+    
+    #QPoint
+    #QPointF
+    @staticmethod
+    def point(value):
+        return '({}, {})'.format(value.x(), value.y())
+    
+    #QLine
+    #QLineF
+    @staticmethod
+    def line(value):
+        return '({}, {}), ({}, {})'.format(value.x1(), value.y1(), value.x2(), value.y2())
+
+    #QRect
+    #QRectF
+    @staticmethod
+    def rect(value):
+        return '({}, {}), {} x {}'.format(value.x(), value.y(), value.width(), value.height())
+
+    #QIcon
+    @staticmethod
+    def icon(value):
+        return value
+    
+    #QPixmap
+    @staticmethod
+    def pixmap(value):
+        return QIcon(value)
+    
+    #QColor
+    @staticmethod
+    def color(value, size = QSize(16, 16)):
+        pixmap = QPixmap(size)
+        pixmap.fill(value)
+        return QIcon(pixmap)
+
+class FormatQType(QTypeToString):
+    #QCursor
+    @staticmethod
+    def icon_qcursor(value):
+        return QTypeToIcon(value)
